8c95d3094e2eb2ed722e7d8cd62820352f7d3ffa
EVE DENISON <evedenison@gmail.com>
2018-07-13 15:45:39 -0700

    Added assets, basic layout started

---
+++ Magic8Ball/Magic8Ball/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Magic8Ball
+//
+//  Created by Eve Denison on 7/13/18.
+//  Copyright © 2018 Eve Denison. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+
+    @IBOutlet weak var eightBallImage: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func askButtonPressed(_ sender: Any) {
+        
+    }
+}
+
